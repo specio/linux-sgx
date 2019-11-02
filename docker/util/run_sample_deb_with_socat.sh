3f7bbb2b04a66185015791f2363dcb3c7e87c824
Haitao Huang <4699115+haitaohuang@users.noreply.github.com>
2019-11-02 17:23:33 -0500

    add docker images with deb installer

---
+++ docker/util/run_sample_deb_with_socat.sh
@@ -0,0 +1,8 @@
+# expecting aesm socket exposed at /tmp/aesm_client on host. For example:
+#
+# ../build_and_run_aesm_docker.sh
+#
+# ./set_up_aesm_socat.sh
+
+# change /dev/isgx to /dev/sgx for DCAP driver
+docker run --env http_proxy --env https_proxy --device=/dev/isgx -v /tmp/aesm_client:/var/run/aesmd -it sgx_sample_deb
